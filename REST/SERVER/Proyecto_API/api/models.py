dae320fe64403d913649605b2c1592c08f7b741e
AaronHuerta025 <codingcode25@gmail.com>
2022-05-23 22:47:31 -0500

    Eliminacion Python rest

--- REST/SERVER/Proyecto_API/api/models.py
+++
@@ -1,9 +0,0 @@
-from pyexpat import model
-from django.db import models
-
-# Create your models here.
-class Users(models.Model):
-    Name=models.CharField(max_length=50)
-    Email=models.CharField(max_length=50)
-    Password=models.CharField(max_length=50)
-    Description=models.CharField(max_length=100)
--- REST/SERVER/Proyecto_API/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import UsersView
-
-urlpatterns=[
-    path('users/',UsersView.as_view(),name='users_list'),
-    path('users/<int:id>',UsersView.as_view(),name='users_process'),
-]
